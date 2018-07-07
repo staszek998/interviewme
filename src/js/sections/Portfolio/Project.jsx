8cf13da15f486f5af9e133c177b30c5d84e93562
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-07-07 23:51:55 +0200

    Correct behaviour of the project description on the touch-screen devices

--- src/js/sections/Portfolio/Project.jsx
+++ src/js/sections/Portfolio/Project.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
 class Project extends Component {
+  hideAllDescriptions = () => {
+    let eventHandler = () => {
+      document
+        .querySelectorAll('.project__description')
+        .forEach(projectDescription => {
+          projectDescription.classList.remove('visible');
+        });
+      document.body.removeEventListener('click', eventHandler);
+    };
+
+    document.body.addEventListener('click', eventHandler);
+  };
+
   render() {
+    let description;
+
     return (
       <div
         className="project"
         style={{ backgroundImage: `url(${this.props.imgUrl})` }}
+        onClick={() => {
+          this.hideAllDescriptions();
+          description.classList.add('visible');
+        }}
       >
-        <div className="project__description">
+        <div
+          className="project__description"
+          ref={node => (description = node)}
+        >
           <a href={this.props.href}>{this.props.title}</a>
           <span>{this.props.subtitle}</span>
         </div>
